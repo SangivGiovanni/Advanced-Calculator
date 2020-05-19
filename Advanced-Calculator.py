edd49b0946aa0e953d7181a232a4050b89d9d951
SangivGiovanni <sangivgiovanni@aol.com>
2020-05-19 17:53:36 +0100

    calculator can now subtract

--- Advanced-Calculator.py
+++ Advanced-Calculator.py
@@ -1,23 +1,40 @@
+# Advanced Calculator Project
+
+
+# Functions to run calculator:
+
+def engine(a_list):
+    num = int(a_list[0])
+    for i, j in enumerate(a_list):
+        if "-" in j and j.endswith("-"):
+            if j.count("-") % 2 == 0:
+                num += int(a_list[i + 1])
+            else:
+                num -= int(a_list[i + 1])
+        if "+" in j:
+            num += int(a_list[i + 1])
+    return num
+
+
+# Running the calculator:
+
 run = True
 
 while run:
 
     a = input()
-    num = 0
     b = []
 
     if " " in a:
         b = [i for i in a.split()]
-        for i in b:
-            num += int(i)
-        print(num)
+        print(engine(b))
 
     elif a == "/exit":
         print("Bye!")
         break
 
     elif a == "/help":
-        print("The program calculates the sum of numbers")
+        print("The program adds and subtracts numbers")
 
     elif a == "":
         continue
